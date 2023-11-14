b5aaf1dcef9fd1977e9554351a9773e305686c66
Alex Greco <alex.k.greco.4@gmail.com>
2023-11-13 20:49:22 -0700

    Added a mono font from google.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Sometype_Mono } from "next/font/google";
 import { ThemeProvider } from "./components/providers";
 import NavBar from "./components/NavBar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+export const sometype = Sometype_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alex Greco",
