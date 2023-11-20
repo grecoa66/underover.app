d52f2c58bdf15c8f17722ca6db1347cbe5c2ec91
Alex Greco <alex.k.greco.4@gmail.com>
2023-11-20 12:01:13 -0700

    Adding keys to shared slate component.

--- app/(under-over)/under-over/manage/slates/components/slate.tsx
+++ app/(under-over)/under-over/manage/slates/components/slate.tsx
@@ -3,7 +3,7 @@ import { FaPen } from "react-icons/fa";
 
 const Slate = ({ slate }: { slate: slates }) => {
   return (
-    <div key={slate.id} className="flex flex-col justify-center p-2">
+    <div className="flex flex-col justify-center p-2">
       <p>League {slate.league}</p>{" "}
       {slate.league === "nfl" && <p>Week {slate.nfl_week}</p>}
     </div>
@@ -15,7 +15,7 @@ const InactiveSlates = ({ slates }: { slates: slates[] }) => {
     <div className="flex w-1/6 flex-col border-2 border-mint p-4">
       <p className="border-b-2 border-mint pb-2">Inactive Slates</p>
       {slates.map((slate) => (
-        <Slate slate={slate} />
+        <Slate key={slate.id} slate={slate} />
       ))}
     </div>
   );
@@ -26,7 +26,7 @@ const ActiveSlates = ({ slates }: { slates: slates[] }) => {
     <div className="flex w-1/6 flex-col border-2 border-mint p-4">
       <p className="border-b-2 border-mint pb-2">Active Slates</p>
       {slates.map((slate) => (
-        <Slate slate={slate} />
+        <Slate key={slate.id} slate={slate} />
       ))}
       {/* TODO: Abstract Button */}
       <div
