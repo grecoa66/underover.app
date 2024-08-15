4596c3179d0920bcce30ec8b82bb3b008285884d
Alex Greco <alex.k.greco.4@gmail.com>
2024-08-14 21:11:25 -0600

    New hook for tracking mouse position. Minimal style changes.

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -56,82 +56,84 @@ export default function Home() {
           <PopText text="Remix" />
         </p>
       </div>
-      <ExperienceGrid
-        date="Jan 2024 - Present"
-        title={"Senior Frontend Engineer - Dust Identity"}
-        description={""}
-        tech={
-          <>
-            <Pill text="Remix" />
-            <Pill text="React" />
-            <Pill text="Typescript" />
-            <Pill text="React Native" />
-            <Pill text="Expo" />
-          </>
-        }
-      />
-      <ExperienceGrid
-        date="Jan 2022 - Oct 2023"
-        title="Senior Fullstack Engineer - UDisc"
-        description={
-          "Contributed to the entire software development life cycle in a full-stack JavaScript environment using Remix, TypeScript, React, Node, and Tailwind. Played a pivotal role in migrating the main web application from Meteor.js to Remix. Implemented server side rendering and optimistic UI to enhance performance and user experience. Worked on diverse projects, including building a comprehensive design system with many reusable components, crafting live interactive scoreboards, writing API endpoints for mobile applications, and implementing robust logging, tracing, and analytics solutions."
-        }
-        tech={
-          <>
-            <Pill text="Remix" />
-            <Pill text="React" />
-            <Pill text="Typescript" />
-            <Pill text="Tailwind" />
-            <Pill text="Mongo" />
-          </>
-        }
-      />
-      <ExperienceGrid
-        date="Jun 2021 - Dec 2021"
-        title="Senior Fullstack Engineer - NorthOne"
-        description={
-          "Built core banking features including debit card activation, wire payments, and calculated properties for analytics. Worked in a Serverless environment hosted on AWS. Built and deployed microservices using Typescript Node Lambdas and DynamoDB."
-        }
-        tech={
-          <>
-            <Pill text="React" />
-            <Pill text="Typescript" />
-            <Pill text="AWS" />
-            <Pill text="Serverless" />
-          </>
-        }
-      />
-      <ExperienceGrid
-        date="Jul 2018 - Jun 2021"
-        title="Senior Software Engineer - Harry's, Inc."
-        description={
-          "Built a globally distributed e-commerce application that delivers blazing fast speeds by taking advantage of server-side rendering and extensive edge caching. Developed a scalable event streaming pipeline, using Terraform and AWS services, to consume and transform purchase data before shipping it off to 3rd party platforms. Helped the Digital Design team create a design system that is shared across all of Harry’s brands. We systematized a shared CSS grid, tokenized spacing and font sizes, and developed accessible color palettes. Migrated the Flamingo web app from Redux state management and Restful APIs to Apollo-Client and Graphql."
-        }
-        tech={
-          <>
-            <Pill text="React" />
-            <Pill text="Typescript" />
-            <Pill text="GatsbyJS" />
-            <Pill text="Styled Components" />
-            <Pill text="AWS" />
-          </>
-        }
-      />
-      <ExperienceGrid
-        date="Jan 2016 - Jun 2018"
-        title="Specialist Application Developer - AT&T"
-        description={
-          "Joined the Technology Development Program as a fresh college graduate. Worked as a full stack developer on the Sales Express application leveraging Vanilla JS, React, Java Spring, and OracleDB. Interviewed and mentored interns and new college graduates."
-        }
-        tech={
-          <>
-            <Pill text="React" />
-            <Pill text="Javascript" />
-            <Pill text="Java Spring" />
-            <Pill text="OracleDB" />
-          </>
-        }
-      />
+      <div className="mt-16">
+        <ExperienceGrid
+          date="Jan 2024 - Present"
+          title={"Senior Frontend Engineer - Dust Identity"}
+          description={""}
+          tech={
+            <>
+              <Pill text="Remix" />
+              <Pill text="React" />
+              <Pill text="Typescript" />
+              <Pill text="React Native" />
+              <Pill text="Expo" />
+            </>
+          }
+        />
+        <ExperienceGrid
+          date="Jan 2022 - Oct 2023"
+          title="Senior Fullstack Engineer - UDisc"
+          description={
+            "Contributed to the entire software development life cycle in a full-stack JavaScript environment using Remix, TypeScript, React, Node, and Tailwind. Played a pivotal role in migrating the main web application from Meteor.js to Remix. Implemented server side rendering and optimistic UI to enhance performance and user experience. Worked on diverse projects, including building a comprehensive design system with many reusable components, crafting live interactive scoreboards, writing API endpoints for mobile applications, and implementing robust logging, tracing, and analytics solutions."
+          }
+          tech={
+            <>
+              <Pill text="Remix" />
+              <Pill text="React" />
+              <Pill text="Typescript" />
+              <Pill text="Tailwind" />
+              <Pill text="Mongo" />
+            </>
+          }
+        />
+        <ExperienceGrid
+          date="Jun 2021 - Dec 2021"
+          title="Senior Fullstack Engineer - NorthOne"
+          description={
+            "Built core banking features including debit card activation, wire payments, and calculated properties for analytics. Worked in a Serverless environment hosted on AWS. Built and deployed microservices using Typescript Node Lambdas and DynamoDB."
+          }
+          tech={
+            <>
+              <Pill text="React" />
+              <Pill text="Typescript" />
+              <Pill text="AWS" />
+              <Pill text="Serverless" />
+            </>
+          }
+        />
+        <ExperienceGrid
+          date="Jul 2018 - Jun 2021"
+          title="Senior Software Engineer - Harry's, Inc."
+          description={
+            "Built a globally distributed e-commerce application that delivers blazing fast speeds by taking advantage of server-side rendering and extensive edge caching. Developed a scalable event streaming pipeline, using Terraform and AWS services, to consume and transform purchase data before shipping it off to 3rd party platforms. Helped the Digital Design team create a design system that is shared across all of Harry’s brands. We systematized a shared CSS grid, tokenized spacing and font sizes, and developed accessible color palettes. Migrated the Flamingo web app from Redux state management and Restful APIs to Apollo-Client and Graphql."
+          }
+          tech={
+            <>
+              <Pill text="React" />
+              <Pill text="Typescript" />
+              <Pill text="GatsbyJS" />
+              <Pill text="Styled Components" />
+              <Pill text="AWS" />
+            </>
+          }
+        />
+        <ExperienceGrid
+          date="Jan 2016 - Jun 2018"
+          title="Specialist Application Developer - AT&T"
+          description={
+            "Joined the Technology Development Program as a fresh college graduate. Worked as a full stack developer on the Sales Express application leveraging Vanilla JS, React, Java Spring, and OracleDB. Interviewed and mentored interns and new college graduates."
+          }
+          tech={
+            <>
+              <Pill text="React" />
+              <Pill text="Javascript" />
+              <Pill text="Java Spring" />
+              <Pill text="OracleDB" />
+            </>
+          }
+        />
+      </div>
     </main>
   );
 }
---
+++ app/hooks/useMousePosition.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState<{
+    x: null | number;
+    y: null | number;
+  }>({
+    x: null,
+    y: null,
+  });
+  useEffect(() => {
+    const updateMousePosition = (ev: MouseEvent) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+  return mousePosition;
+};
+export default useMousePosition;
