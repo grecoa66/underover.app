f280d4a85771be0ee111f428e95e2bebe292856a
Alex Greco <alex.k.greco.4@gmail.com>
2024-08-14 20:49:02 -0600

    Added descriptions to job postings.

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -10,11 +10,11 @@ const ExperienceGrid = ({
 }: {
   date: string;
   title: string;
-  description?: ReactNode;
+  description: string;
   tech: ReactNode;
 }) => {
   return (
-    <div className="my-8 h-fit">
+    <div className="my-8 h-fit max-w-screen-sm">
       <div className="grid grid-cols-4">
         <div className="col-span-1 pt-1 text-sm text-celtic dark:text-mint-400">
           {date}
@@ -23,8 +23,9 @@ const ExperienceGrid = ({
           <h3 className="mb-2 text-lg text-celtic dark:text-mint-400">
             {title}
           </h3>
-          {/* Icon pills for tech I used */}
-          {description ? description : null}
+          <p className="text-sm text-celtic dark:text-mint-400">
+            {description ? description : null}
+          </p>
           <div className="mt-4 flex flex-row flex-wrap gap-2">{tech}</div>
         </div>
       </div>
@@ -50,7 +51,7 @@ export default function Home() {
         </h3>
         <p className="text-lg text-celtic-400 dark:text-everglade-300">
           I&apos;m a software engineer specializing in fullstack{" "}
-          <PopText text="Javscript" /> development. My favorite tools are{" "}
+          <PopText text="Javascript" /> development. My favorite tools are{" "}
           <PopText text="React," /> <PopText text="Typescript" /> and{" "}
           <PopText text="Remix" />
         </p>
@@ -58,6 +59,7 @@ export default function Home() {
       <ExperienceGrid
         date="Jan 2024 - Present"
         title={"Senior Frontend Engineer - Dust Identity"}
+        description={""}
         tech={
           <>
             <Pill text="Remix" />
@@ -71,6 +73,9 @@ export default function Home() {
       <ExperienceGrid
         date="Jan 2022 - Oct 2023"
         title="Senior Fullstack Engineer - UDisc"
+        description={
+          "Contributed to the entire software development life cycle in a full-stack JavaScript environment using Remix, TypeScript, React, Node, and Tailwind. Played a pivotal role in migrating the main web application from Meteor.js to Remix. Implemented server side rendering and optimistic UI to enhance performance and user experience. Worked on diverse projects, including building a comprehensive design system with many reusable components, crafting live interactive scoreboards, writing API endpoints for mobile applications, and implementing robust logging, tracing, and analytics solutions."
+        }
         tech={
           <>
             <Pill text="Remix" />
@@ -85,21 +90,23 @@ export default function Home() {
         date="Jun 2021 - Dec 2021"
         title="Senior Fullstack Engineer - NorthOne"
         description={
-          <p className="text-sm text-celtic-300 dark:text-everglade-300">
-            Full stack developer working on core banking features
-          </p>
+          "Built core banking features including debit card activation, wire payments, and calculated properties for analytics. Worked in a Serverless environment hosted on AWS. Built and deployed microservices using Typescript Node Lambdas and DynamoDB."
         }
         tech={
           <>
             <Pill text="React" />
             <Pill text="Typescript" />
             <Pill text="AWS" />
+            <Pill text="Serverless" />
           </>
         }
       />
       <ExperienceGrid
         date="Jul 2018 - Jun 2021"
         title="Senior Software Engineer - Harry's, Inc."
+        description={
+          "Built a globally distributed e-commerce application that delivers blazing fast speeds by taking advantage of server-side rendering and extensive edge caching. Developed a scalable event streaming pipeline, using Terraform and AWS services, to consume and transform purchase data before shipping it off to 3rd party platforms. Helped the Digital Design team create a design system that is shared across all of Harry’s brands. We systematized a shared CSS grid, tokenized spacing and font sizes, and developed accessible color palettes. Migrated the Flamingo web app from Redux state management and Restful APIs to Apollo-Client and Graphql."
+        }
         tech={
           <>
             <Pill text="React" />
@@ -107,6 +114,21 @@ export default function Home() {
             <Pill text="GatsbyJS" />
             <Pill text="Styled Components" />
             <Pill text="AWS" />
+          </>
+        }
+      />
+      <ExperienceGrid
+        date="Jan 2016 - Jun 2018"
+        title="Specialist Application Developer - AT&T"
+        description={
+          "Joined the Technology Development Program as a fresh college graduate. Worked as a full stack developer on the Sales Express application leveraging Vanilla JS, React, Java Spring, and OracleDB. Interviewed and mentored interns and new college graduates."
+        }
+        tech={
+          <>
+            <Pill text="React" />
+            <Pill text="Javascript" />
+            <Pill text="Java Spring" />
+            <Pill text="OracleDB" />
           </>
         }
       />
