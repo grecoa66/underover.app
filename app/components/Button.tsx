2a63a7515c33483d9bcfd2105b72919067c55abd
Alex Greco <alex.k.greco.4@gmail.com>
2024-10-28 19:54:55 -0600

    Added my components page to the css playground. Made my buttons look better with disabled states.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,25 +5,48 @@ import { twMerge } from "tailwind-merge";
 
 type buttonVariants = "base" | "danger" | "inverse";
 
+/**
+ * The classes being merged in the switch state are order as follows:
+ * - Light mode base colors
+ * - Light mode hover
+ * - Light mode disabled hover
+ * - Dark mode base colors
+ * - Dark mode hover
+ * - Dark mode disabled hover
+ */
 const getButtonStyles = (variant: buttonVariants) => {
   const baseStyles =
     "w-fit flex flex-row items-center justify-center space-x-2 p-2 rounded-lg border-2 disabled:opacity-50 disabled:cursor-not-allowed";
   switch (variant) {
     case "base": {
       return twMerge(
-        "border-everglade text-everglade dark:text-mint hover:bg-everglade hover:text-white dark:border-mint dark:hover:bg-mint dark:hover:text-black",
+        "border-everglade text-everglade",
+        "hover:bg-everglade hover:text-white",
+        "disabled:hover:bg-transparent disabled:hover:text-everglade",
+        "dark:border-mint dark:text-mint",
+        "dark:hover:bg-mint dark:hover:text-black",
+        "disabled:dark:hover:bg-transparent disabled:dark:hover:text-mint",
         baseStyles,
       );
     }
     case "danger": {
       return twMerge(
-        "border-red-400 text-red-400 hover:bg-red-400 hover:text-white",
+        "border-red-500 text-red-500",
+        "hover:bg-red-500 hover:text-white",
+        "disabled:hover:bg-transparent disabled:hover:text-red-500",
+        "dark:hover:text-black",
+        "disabled:dark:hover:text-red-500",
         baseStyles,
       );
     }
     case "inverse": {
       return twMerge(
-        "bg-everglade border-everglade text-white hover:bg-white hover:text-everglade dark:border-mint dark:bg-mint dark:text-black dark:hover:bg-black dark:hover:text-mint",
+        "bg-everglade border-everglade text-white",
+        "hover:bg-transparent hover:text-everglade",
+        "disabled:hover:bg-everglade disabled:hover:text-white",
+        "dark:border-mint dark:bg-mint dark:text-black",
+        "dark:hover:bg-transparent dark:hover:text-mint ",
+        "disabled:dark:hover:bg-mint disabled:dark:hover:text-black",
         baseStyles,
       );
     }
