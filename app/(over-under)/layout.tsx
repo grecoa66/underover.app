24880688960ac267353a12f62d6bdcdb4788bf1f
Alex Greco <alex.k.greco.4@gmail.com>
2024-11-15 16:04:44 -0700

    Meta tag for google verficiation.

--- app/(over-under)/layout.tsx
+++ app/(over-under)/layout.tsx
@@ -8,6 +8,12 @@ const sometype = Sometype_Mono({ subsets: ["latin"] });
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="5TYR8xGUmH-zksOUZKsGEbWbWEXk0_fb6FvBnowpv4A"
+        />
+      </head>
       <body className={sometype.className}>
         <AppProvider attribute="class">
           <UONavBar />
