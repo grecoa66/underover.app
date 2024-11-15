93638ce5413a028bb8507cb9962cf4683e01ca4b
Alex Greco <alex.k.greco.4@gmail.com>
2024-11-14 18:55:32 -0700

    Reorg of some components in the wrong places.

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -1,53 +1,8 @@
 import "../components/Header.css";
-import { Pill } from "../(over-under)/components/Pill";
-import { ReactNode } from "react";
-import { SocialLinks } from "../(over-under)/components/SocialLinks";
-import { BrandIcon } from "../(over-under)/components/BrandIcon";
-
-const ExperienceGrid = ({
-  date,
-  title,
-  description,
-  tech,
-}: {
-  date: string;
-  title: string;
-  description: string;
-  tech: ReactNode;
-}) => {
-  return (
-    <div className="z-0 my-8 h-fit max-w-screen-sm">
-      <div className="hidden grid-cols-4 rounded-md p-4 transition-colors duration-500 ease-in-out hover:bg-mint-500 hover:bg-opacity-20 hover:drop-shadow-sm dark:hover:bg-black-200 dark:hover:bg-opacity-20 md:grid">
-        <div className="col-span-1 pt-1 text-sm text-celtic dark:text-mint-400">
-          {date}
-        </div>
-        <div className="col-span-3">
-          <h3 className="mb-2 text-lg text-celtic dark:text-mint-400">
-            {title}
-          </h3>
-          <p className="text-sm text-celtic dark:text-mint-400">
-            {description ? description : null}
-          </p>
-          <div className="mt-4 flex flex-row flex-wrap gap-2">{tech}</div>
-        </div>
-      </div>
-      <div className="md:hidden">
-        <div className="col-span-3">
-          <h3 className="mb-2 text-lg text-celtic dark:text-mint-400">
-            {title}
-          </h3>
-          <div className="col-span-1 py-2 text-sm text-celtic dark:text-mint-400">
-            {date}
-          </div>
-          <p className="text-sm text-celtic dark:text-mint-400">
-            {description ? description : null}
-          </p>
-          <div className="mt-4 flex flex-row flex-wrap gap-2">{tech}</div>
-        </div>
-      </div>
-    </div>
-  );
-};
+import { Pill } from "../components/Pill";
+import { SocialLinks } from "../components/SocialLinks";
+import { AKGIcon } from "../components/AKGIcon";
+import { ExperiencePanel } from "../components/ExperiencePanel";
 
 export default function Home() {
   const PopText = ({ text }: { text: string }) => (
@@ -77,7 +32,7 @@ export default function Home() {
         <h3 className="text-xl text-celtic-200 dark:text-mint-400">
           Experience
         </h3>
-        <ExperienceGrid
+        <ExperiencePanel
           date="Jan 2024 - Present"
           title={"Senior Frontend Engineer - Dust Identity"}
           description={
@@ -93,7 +48,7 @@ export default function Home() {
             </>
           }
         />
-        <ExperienceGrid
+        <ExperiencePanel
           date="Jan 2022 - Oct 2023"
           title="Senior Fullstack Engineer - UDisc"
           description={
@@ -109,7 +64,7 @@ export default function Home() {
             </>
           }
         />
-        <ExperienceGrid
+        <ExperiencePanel
           date="Jun 2021 - Dec 2021"
           title="Senior Fullstack Engineer - NorthOne"
           description={
@@ -124,7 +79,7 @@ export default function Home() {
             </>
           }
         />
-        <ExperienceGrid
+        <ExperiencePanel
           date="Jul 2018 - Jun 2021"
           title="Senior Software Engineer - Harry's, Inc."
           description={
@@ -140,7 +95,7 @@ export default function Home() {
             </>
           }
         />
-        <ExperienceGrid
+        <ExperiencePanel
           date="Jan 2016 - Jun 2018"
           title="Specialist Application Developer - AT&T"
           description={
@@ -156,7 +111,7 @@ export default function Home() {
           }
         />
       </div>
-      <BrandIcon className="my-8" />
+      <AKGIcon className="my-8" />
     </main>
   );
 }
